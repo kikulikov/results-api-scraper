122bd948160e0ee9a8597ff657a25b78d3973d3d
Kirill Kulikov <kirill.kulikov@gmail.com>
2016-02-16 22:00:09 +0000

    Tail command

--- api-mole.go
+++ api-mole.go
@@ -4,15 +4,41 @@ import (
 	"flag"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 // http://marcio.io/2015/07/supercharging-atom-editor-for-go-development/
 
 const rawurl = "https://qa-api.efset.org/test-results"
+const dateFormat = "2006-01-02T15:04:05.000Z07:00"
 
 var secretKey string
 var fromDate string
 var toDate string
+var tailInterval int
+
+func main() {
+	flag.StringVar(&secretKey, "key", "", "API Key")
+	flag.StringVar(&fromDate, "from", "", "Date FROM")
+	flag.StringVar(&toDate, "to", "", "Date TO")
+	flag.IntVar(&tailInterval, "tail", 0, "Tail Interval (s)")
+	flag.Parse()
+
+	WriteHeaders()
+
+	if tailInterval > 0 {
+		var now = time.Now().UTC()
+		var begin = now.Add(time.Duration(-1*tailInterval) * time.Second).Format(dateFormat)
+		var end = now.Format(dateFormat)
+
+		for {
+			callAPI(PrepareRequest(rawurl, begin, end, secretKey))
+			time.Sleep(time.Duration(tailInterval) * time.Second)
+		}
+	} else {
+		callAPI(PrepareRequest(rawurl, fromDate, toDate, secretKey))
+	}
+}
 
 func callAPI(req http.Request) {
 	resp := MakeRequest(&req)
@@ -39,14 +65,4 @@ func callAPI(req http.Request) {
 			callAPI(PrepareRequestWhenFullURL(parsed.Next, secretKey))
 		}
 	}
-}
-
-func main() {
-	flag.StringVar(&secretKey, "key", "", "API Key")
-	flag.StringVar(&fromDate, "from", "", "Date FROM")
-	flag.StringVar(&toDate, "to", "", "Date TO")
-	flag.Parse()
-
-	WriteHeaders()
-	callAPI(PrepareRequest(rawurl, fromDate, toDate, secretKey))
 }
